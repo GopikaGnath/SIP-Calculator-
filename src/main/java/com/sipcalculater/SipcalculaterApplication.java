c817d83ffa229c6c8fb97ebe94d04d55ffac6297
GopikaGnath <gopikagnath97@gmail.com>
2024-03-15 12:54:36 +0530

    first commit

---
+++ src/main/java/com/sipcalculater/SipcalculaterApplication.java
@@ -0,0 +1,13 @@
+package com.sipcalculater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipcalculaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipcalculaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sipcalculater/SipcalculaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipcalculater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipcalculaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
